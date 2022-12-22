2d0b3fc62761978f19222269ecff6e962425fe5d
Леонид Ягупов <lyagupov@yandex.ru>
2022-12-22 17:54:54 +0300

    fixed sender

--- internal/agent/utils/sender/sender.go
+++ internal/agent/utils/sender/sender.go
@@ -12,39 +12,42 @@ import (
 	"github.com/popooq/collectimg-ma/internal/utils/encoder"
 )
 
+var encoderJSON encoder.Metrics
+
 func SendMetrics(metricData collector.MetricsMap) {
 	for k, v := range metricData {
-		var encoder encoder.Metrics
+
 		fmt.Println("")
 		fmt.Println("")
 		fmt.Println("")
 		fmt.Println("")
 		types := strings.ToLower(strings.TrimPrefix(fmt.Sprintf("%T", v), "collector."))
 
-		encoder.ID = k
-		encoder.MType = types
-		if encoder.MType == "gauge" {
+		encoderJSON.ID = k
+		encoderJSON.MType = types
+		if encoderJSON.MType == "gauge" {
 			assertvalue, ok := v.(collector.Gauge)
 			if !ok {
 				log.Printf("conversion failed")
 			}
 			floatvalue := float64(assertvalue)
-			encoder.Value = &floatvalue
+			encoderJSON.Value = &floatvalue
+			encoderJSON.Delta = nil
 		}
-		if encoder.MType == "counter" {
+		if encoderJSON.MType == "counter" {
 			assertdelta, ok := v.(collector.Counter)
 			if !ok {
 				log.Printf("conversion failed")
 			}
 			intdelta := int64(assertdelta)
-			encoder.Delta = &intdelta
-
+			encoderJSON.Delta = &intdelta
+			encoderJSON.Value = nil
 		}
-		body, err := encoder.Marshall()
+		body, err := encoderJSON.Marshall()
 		if err != nil {
 			log.Printf("error %s in agent", err)
 		}
-		log.Printf("struct %+v", encoder)
+		log.Printf("struct %+v", encoderJSON)
 		endpoint := "http://127.0.0.1:8080/update/"
 		resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(body))
 		if err != nil {
