f16ad44e6d79dc6094a3a6e91928eae0f43f8990
Леонид Ягупов <lyagupov@yandex.ru>
2023-05-12 19:12:21 +0300

    fix

--- internal/utils/encryptor/encryptor.go
+++ internal/utils/encryptor/encryptor.go
@@ -6,7 +6,6 @@ import (
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
 	"log"
 	"os"
 )
@@ -59,27 +58,26 @@ func (e *Encryptor) GetKey(pathKey string, keyType string) (err error) {
 
 func (e *Encryptor) Encrypt(message []byte) (encryptString []byte, err error) {
 
-	if e.publicKey == nil {
-		err := fmt.Errorf("there is no public key")
-		return message, err
+	if e.publicKey != nil {
+
+		hash := sha256.New()
+		label := []byte("")
+
+		encryptString, err = rsa.EncryptOAEP(hash, rand.Reader, e.publicKey, message, label)
+		if err != nil {
+			return []byte(""), nil
+		}
+
+		return encryptString, nil
 	}
+	return message, nil
 
-	hash := sha256.New()
-	label := []byte("")
-
-	encryptString, err = rsa.EncryptOAEP(hash, rand.Reader, e.publicKey, message, label)
-	if err != nil {
-		return []byte(""), nil
-	}
-
-	return encryptString, nil
 }
 
 func (e *Encryptor) Decrypt(message []byte) (encryptString []byte, err error) {
 
 	if e.privateKey == nil {
-		err := fmt.Errorf("there is no private key")
-		return message, err
+		return message, nil
 	}
 
 	hash := sha256.New()
